95cf8051bc41785ef453d673acb33c9c2208663d
Andrew2023 <83838963+userAndrew2023@users.noreply.github.com>
2023-07-01 13:18:20 +0300

    C#

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите массив строк, разделяя элементы запятыми:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(',');
+
+        string[] filteredArray = FilterStrings(inputArray);
+
+        Console.WriteLine("Результат:");
+        foreach (string str in filteredArray)
+        {
+            Console.Write(str + " ");
+        }
+
+    }
+
+    static string[] FilterStrings(string[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        int index = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                result[index] = array[i];
+                index++;
+            }
+        }
+
+        return result;
+    }
+}
